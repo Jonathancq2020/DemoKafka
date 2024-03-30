f804d6ae4e5cef7aab5c83886e53f1a526036d56
Jonathan <70780570+Jonathancq2020@users.noreply.github.com>
2024-03-30 01:37:24 -0500

    Add DemoKafka.PersistenceEFCore

---
+++ ProyectoDemoKafka/DemoKafka.PersistenceEFCore/DataContexts/DemoKafkaContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DemoKafka.PersistenceEFCore.DataContexts
+{
+    internal class DemoKafkaContextFactory :
+        IDesignTimeDbContextFactory<DemoKafkaContext>
+    {
+        //Add-Migration AddDemoKafka -p DemoKafka.PersistenceEFCore -s DemoKafka.PersistenceEFCore -c DemoKafkaContextFactory
+        //Update-Database -p DemoKafka.PersistenceEFCore -s DemoKafka.PersistenceEFCore -context DemoKafkaContextFactory
+        public DemoKafkaContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder =
+            new DbContextOptionsBuilder<DemoKafkaContext>();
+
+            OptionBuilder
+                .UseMySQL("server=localhost;port=3307;database=TareaKafka;user=root;password=DemoKafka#2024");
+
+            return new DemoKafkaContext(OptionBuilder.Options);
+        }
+    }
+}
---
+++ ProyectoDemoKafka/DemoKafka.PersistenceEFCore/Repositories/RegisterNotificationMessageRepository.cs
@@ -0,0 +1,27 @@
+﻿using DemoKafka.DomainServices.Interfaces.Repositories;
+using DemoKafka.PersistenceEFCore.DataContexts;
+using NotificationMessage = DemoKafka.PersistenceEFCore.Entities.NotificationMessage;
+
+namespace DemoKafka.PersistenceEFCore.Repositories
+{
+    internal class RegisterNotificationMessageRepository : IRegisterNotificationMessageRepository
+    {
+        readonly DemoKafkaContext Context;
+
+        public RegisterNotificationMessageRepository(DemoKafkaContext context)
+        {
+            Context = context;
+        }
+
+        public async Task Register(DomainModel.ValueObjects.NotificationMessage notificationMessage)
+        {
+            await Context.NotificationMessages.AddAsync(new NotificationMessage
+            {
+                Message = notificationMessage.Message,
+                CreatedDate = notificationMessage.CreatedDate
+            });
+
+            await Context.SaveChangesAsync();
+        }
+    }
+}

a5ed38fae556cb50227eacf681a773dfc434f1de
Jonathan <70780570+Jonathancq2020@users.noreply.github.com>
2024-03-30 01:42:23 -0500

    Add DemoKafka.WebApi

---
+++ ProyectoDemoKafka/DemoKafka.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using DemoKafka.Application.Dtos.RegisterOrder;
+using DemoKafka.WebApi.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoKafka.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class OrderController : ControllerWrapper
+    {
+
+        [HttpPost("register")]
+        public async Task<IActionResult> register(RegisterOrderRequestDto request)
+        {
+            return Ok(await Mediator.Send(request));
+        }
+    }
+}
